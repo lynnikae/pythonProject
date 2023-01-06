2b21ab773ab31b0c723429a98d634b6f03d38241
lynni <lynnikanre604@gmail.com>
2023-01-06 15:21:24 +0300

    Sec commit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 #     if len(s[i] + "Y" + s[i + 1]) > max_len:
 #         max_len = len(s[i] + "Y" + s[i + 1])
 # print(max_len)
+from pprint import pprint
 
 # f = open('5.txt')
 # s = f.readline().replace("CDK", "CD_DK").replace("CD", "0").replace("DK", "0")
@@ -87,8 +88,80 @@
 # print(min(lens))
 
 
+# f = open('1.txt')
+# s = f.readline()
+# cnt, maxcnt = 1, 0
+# for i in range(len(s) - 1):
+#     if s[i] != s[i + 1]:
+#         cnt += 1
+#         maxcnt = max(cnt, maxcnt)
+#     else:
+#         maxcnt = max(cnt, maxcnt)
+#         cnt = 1
+# print(maxcnt)
 
 
+# f = open("2.txt")
+# s = f.readline()
+# cnt = 1
+# max_cnt = 1
+# for i in range(len(s) - 1):
+#     if s[i] > "5" and s[i + 1] > "5" and s[i] >= s[i + 1]:
+#         cnt += 1
+#         max_cnt = max(max_cnt, cnt)
+#     else:
+#         max_cnt = max(max_cnt, cnt)
+#         cnt = 1
+# print(max_cnt)
+
+
+# f = open("3.txt")
+# s = f.readline()
+# cnt, maxcnt = 1, 1
+# for i in range(len(s) - 1):
+#     if s[i] in "AEOI" and s[i + 1] in "AEOI" and s[i] <= s[i + 1]:
+#         cnt += 1
+#         maxcnt = max(maxcnt, cnt)
+#     else:
+#         maxcnt = max(maxcnt, cnt)
+#         cnt = 1
+# print(maxcnt)
+
+
+# f = open("4.txt")
+# s = f.readline()
+# cnt, maxcnt = 1, 1
+# for i in range(len(s) - 1):
+#     if s[i] in "02468" and s[i + 1] in "13579" or s[i + 1] in "02468" and s[i] in "13579":
+#         cnt += 1
+#         maxcnt = max(maxcnt, cnt)
+#     else:
+#         maxcnt = max(maxcnt, cnt)
+#         cnt = 1
+# print(maxcnt)
+
+
+# f = open("5.txt")
+# s = f.readline()
+# cnt, maxcnt = 1, 1
+# for i in range(len(s) - 1):
+#     if s[i] in "02468" and s[i + 1] in "2468":
+#         cnt += 1
+#     elif s[i] in "02468" and s[i + 1] == "0":
+#         cnt += 1
+#         maxcnt = max(maxcnt, cnt)
+#     else:
+#         cnt = 1
+# print(maxcnt)
+
+
+# f = open("24_5.txt")
+# s = f.readline()
+# chisla = []
+# for i in range(len(s)):
+#     if s[i - 1].isalpha() and s[i].isdigit() and s[i + 1].isdigit() and s[i + 2].isdigit() and s[i + 3].isdigit() and s[i + 4].isdigit() and s[i + 5].isdigit() and s[i + 6].isalpha():
+#         chisla.append(s[i] + s[i+1] + s[i+2] + s[i+3] + s[i+4] + s[i+5])
+# print(sorted(chisla))
 
 
 
--- mainn.py
+++ mainn.py
@@ -1,6 +1,11 @@
-def main():
-    print('My first git program')
-
-
-if __name__ == '__main__':
-    main()
+f = open("24_6.txt")
+s = f.readline()
+isnumber = []
+for i in range(len(s)):
+    if s[i] == "8" and s[i + 1].isdigit() and s[i + 2].isdigit() and s[i + 3].isdigit() and s[i + 4].isdigit() and s[
+        i + 5].isdigit() and s[i + 6].isdigit() and s[i + 7].isdigit() and s[i + 8].isdigit() and s[i + 9].isdigit() and \
+            s[i + 10].isdigit() and s[i + 11].isalpha() and (int(s[i + 9]) + int(s[i + 10]) % 2 == 0):
+        isnumber.append(
+            s[i] + s[i + 1] + s[i + 2] + s[i + 3] + s[i + 4] + s[i + 5] + s[i + 6] + s[i + 7] + s[i + 8] + s[i + 9] + s[
+                i + 10])
+print(isnumber)
